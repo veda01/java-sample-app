a982b9896dbc4a83664c51596e68e4a345a9fd78
veda01 <91822763+veda01@users.noreply.github.com>
2021-10-06 22:49:33 +0530

    Update Greeter.java

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -2,6 +2,6 @@ package hello;
 
 public class Greeter {
     public String sayHello() {
-        return "This is banking app and devops practice  ";
+        return "This is banking app and devops practice, you are welcome here ";
     }
 }
